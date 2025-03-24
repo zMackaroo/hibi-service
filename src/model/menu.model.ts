01a61f1c91da7b878dd290aa59f156e1fc80d6ea
syd-enciso <sydney.enciso@snsofttech.ph>
2025-03-24 10:46:01 +0800

    test

--- src/model/menu.model.ts
+++ src/model/menu.model.ts
@@ -1,37 +1,30 @@
 import mongoose from "mongoose";
 
-const menuModel = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    image: {
-      type: String,
-      required: true,
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    isActive: {
-      type: Boolean,
-      required: true,
-    },
+const menuModel = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  {
-    capped: { size: 1024 },
-    bufferCommands: false,
-    autoCreate: false, // disable `autoCreate` since `bufferCommands` is false
-  }
-);
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+  },
+});
 
 export const menuSchema = mongoose.model("menu", menuModel);
--- src/model/order.model.ts
+++ src/model/order.model.ts
@@ -1,33 +1,26 @@
 import mongoose from "mongoose";
 
-const orderModel = new mongoose.Schema(
-  {
-    customerName: {
-      type: String,
-      required: true,
-    },
-    customerPhone: {
-      type: String,
-      required: true,
-    },
-    orders: {
-      type: Array,
-      required: true,
-    },
-    totalPrice: {
-      type: Number,
-      required: true,
-    },
-    status: {
-      type: String,
-      required: true,
-    },
+const orderModel = new mongoose.Schema({
+  customerName: {
+    type: String,
+    required: true,
   },
-  {
-    capped: { size: 1024 },
-    bufferCommands: false,
-    autoCreate: false, // disable `autoCreate` since `bufferCommands` is false
-  }
-);
+  customerPhone: {
+    type: String,
+    required: true,
+  },
+  orders: {
+    type: Array,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+});
 
 export const orderSchema = mongoose.model("order", orderModel);
